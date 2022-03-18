5c9231523f38fb173adee50de9a151d8ad04c115
meomnzak <45256987+meomnzak@users.noreply.github.com>
2022-03-18 19:02:29 +0000

    Sync LeetCode submission - Strobogrammatic Number (cpp)

--- my-folder/problems/strobogrammatic_number/solution.cpp
+++ my-folder/problems/strobogrammatic_number/solution.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     bool isStrobogrammatic(string num) {
         unordered_map<char,char> mp;
-        mp['0'] = '0';
-        mp['1'] = '1';
         mp['6'] = '9';
         mp['9'] = '6';
+        mp['1'] = '1';
+        mp['0'] = '0';
         mp['8'] = '8';
-        int n = num.size();
-        for(int i = 0 ; i<=n/2 ;i++){
-            if(mp.find(num[i])==mp.end()){
-                return false;
-            }else{
-                char expect = mp[num[i]];
-                if(expect!=num[n-i-1]) return false;
-            }
+        for(int i = 0 ; i<=num.size()/2 ; i++){
+            if(mp.find(num[i])==mp.end()) return false;
+            if(mp[num[i]]!=num[num.size()-i-1]) return false;
         }
         return true;
     }
