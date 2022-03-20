209d0a818270ae878a157a7c8e6999bbe54184f0
meomnzak <45256987+meomnzak@users.noreply.github.com>
2022-03-20 16:02:02 +0000

    Sync LeetCode submission - Delete Nodes And Return Forest (cpp)

--- my-folder/problems/delete_nodes_and_return_forest/solution.cpp
+++ my-folder/problems/delete_nodes_and_return_forest/solution.cpp
@@ -12,33 +12,38 @@
 class Solution {
 public:
     vector<TreeNode *> vec;
-    unordered_map<int,int> mp;
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        unordered_map<int,int> mp;
         for(int i = 0 ; i<to_delete.size() ; i++){
             mp[to_delete[i]] = 1;
         }
-        dfs(root);
-        if(root && mp.find(root->val)==mp.end())
+        dfs(root,mp);
+        if(!mp[root->val])
             vec.push_back(root);
+        
         return vec;
     }
-    void dfs(TreeNode *root){
-        if(root==NULL) return;
-        dfs(root->left);
-        dfs(root->right);
-        if(root->right && mp.find(root->right->val)!=mp.end()){
-            root->right = NULL;
+    
+    void dfs(TreeNode *cur, unordered_map<int,int> &mp){
+        if(cur==NULL) return;
+        if(cur->left){
+            dfs(cur->left,mp);
+            if(mp[cur->left->val])
+                cur->left = NULL;
         }
-        if(root->left && mp.find(root->left->val)!=mp.end()){
-            root->left = NULL;
+        if(cur->right){
+            dfs(cur->right,mp);
+            if(mp[cur->right->val])
+                cur->right = NULL;
         }
-        if(mp.find(root->val)!=mp.end()){
-            if(root->left && mp.find(root->left->val)==mp.end())
-                vec.push_back(root->left);
-            if(root->right && mp.find(root->right->val)==mp.end())
-                vec.push_back(root->right);
-            root->left = NULL;
-            root->right= NULL;
+        if(mp[cur->val]){
+            if(cur->left && !mp[cur->left->val])
+                vec.push_back(cur->left);
+            
+            if(cur->right && !mp[cur->right->val])
+                vec.push_back(cur->right);
+            
         }
     }
+    
 };
