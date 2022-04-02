8047270c00eb40be92fa969fb264068a96fdc296
meomnzak <45256987+meomnzak@users.noreply.github.com>
2022-04-02 13:27:36 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- my-folder/problems/maximum_subarray/solution.cpp
+++ my-folder/problems/maximum_subarray/solution.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     
-    const static int N = 1e5+7;
-    
-    int meme[N][3];
-    
-    int solve(int idx, bool can,vector<int> &nums){
-        if(idx==nums.size()){
-            return 0;
-        }
-        int &ret = meme[idx][can];
-        if(~ret) return ret;
-        ret = 0;
-        ret = solve(idx+1,0,nums);
-        if(can){
-            ret = max({ret,nums[idx]+solve(idx+1,1,nums)});
-        }
-        return ret;
-    }
-    
+
     int maxSubArray(vector<int>& nums) {
-        memset(meme,-1,sizeof(meme));
-        int ans = -1e9;
-        bool pos = false;
+        int best = -1e9;
+        int cur = 0;
         for(int i = 0 ; i<nums.size() ; i++){
-            if(nums[i]>=0) pos = true;
-            ans = max(ans,solve(i,1,nums));
+            cur = max(cur+nums[i],nums[i]);
+            best = max(best,cur);
         }
-        
-        if(!pos){
-            ans = *max_element(nums.begin(),nums.end());
-        }
-        
-        
-        return ans;
+        return best;
     }
 };
