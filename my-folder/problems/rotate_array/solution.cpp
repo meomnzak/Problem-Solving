42b76fb65618170218d1c6838534203a1c664389
Moamen Zakaria <45256987+meomnzak@users.noreply.github.com>
2023-03-06 02:59:25 +0200

    Delete my-folder directory

--- my-folder/problems/rotate_array/solution.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        for(;k>0;k--){
-            nums.insert(nums.begin(),nums[nums.size()-1]);
-            nums.pop_back();
-        }
-    }
-};
